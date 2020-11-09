bbaabd81e6217941b92c42b842de8e9e476b1512
AdhirajRanjan <71758327+AdhirajRanjan@users.noreply.github.com>
2020-11-09 19:49:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -74,7 +74,7 @@ function draw() {
 
 function mouseDragged() 
 {
-    Matter.Body.setPosition(bird.body, {x:mouseX,y:mouseY})
+    Matter.Body.setPosition(this.polygon.body, {x:mouseX,y:mouseY})
 }
 
 function mouseReleased() 
